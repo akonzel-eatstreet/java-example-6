8d0084e8079d841952d79687028fad48b09827ea
tony.konzel <tony.konzel@eatstreet.com>
2021-04-20 09:58:51 -0500

    Yay it's a project.

---
+++ src/main/java/com/konzel/example2/Boot.java
@@ -0,0 +1,19 @@
+package com.konzel.example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Boot {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello world";
+    }
+}
